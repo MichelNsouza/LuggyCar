58c95e4d8296248e715b0a84f3da6f436fa5189a
Michel Souza <michelnsouza98@gmail.com>
2024-09-14 10:05:06 -0300

    maven init

---
+++ src/main/java/com/br/luggycar/api/ApiApplication.java
@@ -0,0 +1,14 @@
+package com.br.luggycar.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
