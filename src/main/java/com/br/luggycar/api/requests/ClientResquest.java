fba5e7ca26d9f60f072d1e7089a0fb2af7be49d2
Railan Ibraim <railanibraim3@gmail.com>
2024-09-18 20:11:41 -0300

    commit inicial Client
    subindo Client

---
+++ src/main/java/com/br/luggycar/api/requests/ClientResquest.java
@@ -0,0 +1,27 @@
+package com.br.luggycar.api.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClientResquest {
+
+    private String status;
+    private String name;
+    private String lastName;
+    private String cpf;
+    private String email;
+    private String gender;
+    private Date date;
+    private LocalDate registration;
+}
