fce9b164dd5c7fbf04164e4b672b0aecadbfc136
Thiago Sales <thiagosales46@gmail.com>
2024-09-27 15:44:41 -0300

    feat: adicionado controller/category

---
+++ src/main/java/com/br/luggycar/api/controllers/CategoryController.java
@@ -0,0 +1,45 @@
+package com.br.luggycar.api.controllers;
+
+
+import com.br.luggycar.api.entities.category.Category;
+import com.br.luggycar.api.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/category")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping
+    public List<Category> getAllCategories() {
+        return categoryService.getAllCategories();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> getCategoryById(@PathVariable long id) {
+        return ResponseEntity.ok(categoryService.findById(id));
+    }
+
+    @GetMapping("/{name}")
+    public ResponseEntity<Category> getCategoryByName(@PathVariable String name) {
+        return ResponseEntity.ok(categoryService.findByName(name));
+    }
+    @PostMapping
+    public ResponseEntity<Category> saveCategory(@RequestBody Category category) {
+        return ResponseEntity.ok(categoryService.SaveCategory(category));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Boolean> deleteCategory(@PathVariable long id) {
+        return ResponseEntity.ok(categoryService.deleteCategory(id));
+    }
+
+
+
+}
