39afd4d719c1553ffc3a3f0c3d69c289acfd1123
eldersb <122701368+eldersb@users.noreply.github.com>
2024-10-08 19:42:15 -0300

    Criando tabelas de migração

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -1,7 +1,14 @@
 
 CREATE TABLE accident (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    severity VARCHAR(120) NOT NULL,
-    description TEXT,
+    id BIGINT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    severity ENUM('HIGH', 'LOW', 'MEDIUM'),
+    description VARCHAR(250),
     registration_date DATETIME NOT NULL
 );
+
+CREATE TABLE optional (
+        id BIGINT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+        name VARCHAR(255),
+        quantity_available FLOAT(53) NOT NULL,
+        rental_value FLOAT(53) NOT NULL,
+ );
---
+++ src/main/resources/db/migration/V2__populate_tables.sql
@@ -0,0 +1,9 @@
+INSERT INTO accident (severity, description, registration_date) VALUES
+('LOW', 'Accidente leve, sem feridos.'),
+('MEDIUM', 'Accidente moderado com alguns feridos.'),
+('HIGH', 'Accidente com danos significativos.');
+
+INSERT INTO optional_item (name, quantity_available, rental_value) VALUES
+('Cadeira', 10, 15.50),
+('Mesa', 5, 35.00),
+('Projetor', 3, 75.00);
