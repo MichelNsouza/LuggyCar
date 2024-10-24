f12ae4d97733d01f71041367f889866ea7f8d063
eldersb <122701368+eldersb@users.noreply.github.com>
2024-10-24 14:00:32 -0300

    Ajustando exceptions

--- src/main/java/com/br/luggycar/api/repositories/CategoryRepository.java
+++ src/main/java/com/br/luggycar/api/repositories/CategoryRepository.java
@@ -6,6 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-//    ThreadLocal<Object> findByName(String name);
      Optional<Category> findByName(String name);
 }
