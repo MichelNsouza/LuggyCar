46db2522b75002e243a19e381b4338027f9c06e8
Railan Ibraim <railanibraim3@gmail.com>
2024-10-30 20:26:55 -0300

    restrições sinistro e opcional

--- src/main/java/com/br/luggycar/api/entities/OptionalItem.java
+++ src/main/java/com/br/luggycar/api/entities/OptionalItem.java
@@ -1,9 +1,7 @@
 package com.br.luggycar.api.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,6 +15,8 @@ public class OptionalItem {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
+    @Column(unique = true)
     private String name;
     private double rentalValue;
     private double quantityAvailable;
