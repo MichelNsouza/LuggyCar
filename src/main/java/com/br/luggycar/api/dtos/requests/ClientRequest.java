6032742d5653b235ad36f09dae90a3f59bc866d5
Michel Souza <Michelnsouza98@gmail.com>
2024-11-01 19:38:20 -0300

    feat: cpf != cnpj

--- src/main/java/com/br/luggycar/api/dtos/requests/ClientRequest.java
+++ src/main/java/com/br/luggycar/api/dtos/requests/ClientRequest.java
@@ -12,28 +12,25 @@ import org.hibernate.validator.constraints.br.CPF;
 import java.util.Date;
 
 public record ClientRequest(
+
         PersonType personType,
 
         @Size(min = 2, max = 100, message = "O nome deve ter entre 2 e 100 caracteres.")
         String naturalPersonName,
-
         @CPF(message = "CPF inválido")
         String cpf,
+        @LegalAge
+        @Past(message = "A data de nascimento deve estar no passado.")
+        Date dateBirth,
+        Gender gender,
 
         @CNPJ(message = "CNPJ inválido")
         String cnpj,
-
         @Size(min = 2, max = 150, message = "O nome deve ter entre 2 e 150 caracteres.")
         String companyName,
 
         @Email(message = "Email inválido")
         String email,
-
-        Gender gender,
-
-        @LegalAge
-        @Past(message = "A data de nascimento deve estar no passado.")
-        Date dateBirth,
         String cep,
         @Size(min = 5, max = 300, message = "O endereço deve ter entre 3 e 300 caracteres.")
         String endereco
@@ -43,10 +40,14 @@ public record ClientRequest(
 
     if (cpf != null) {
         cpf = cpf.replace(".", "").replace("-", "");
+        cnpj = null;
+        companyName = null;
     }
 
     if (cnpj != null) {
         cnpj = cnpj.replace(".", "").replace("-", "").replace("/", "");
+        cpf = null;
+        gender = null;
         }
     }
 
