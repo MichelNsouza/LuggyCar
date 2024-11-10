1cf2cd233a9c5271e9dd80bcb852013a32647c5a
eldersb <122701368+eldersb@users.noreply.github.com>
2024-11-10 18:18:34 -0300

    Fix: Problemas de migração

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -1,24 +1,8 @@
-CREATE TABLE accident (
-  id BIGINT AUTO_INCREMENT NOT NULL,
-   severity VARCHAR(255) NULL,
-   `description` VARCHAR(255) NULL,
-   registration_date datetime NULL,
-   CONSTRAINT pk_accident PRIMARY KEY (id)
-);
-
-CREATE TABLE optional_item (
-  id BIGINT AUTO_INCREMENT NOT NULL,
-   name VARCHAR(255) NULL,
-   rental_value DOUBLE NOT NULL,
-   quantity_available DOUBLE NOT NULL,
-   CONSTRAINT pk_optionalitem PRIMARY KEY (id)
-);
 
 CREATE TABLE category (
   id BIGINT AUTO_INCREMENT NOT NULL,
    name VARCHAR(255) NOT NULL,
-   description VARCHAR(255) NULL,
-   image VARCHAR(255) NULL,
+   `description` VARCHAR(255) NULL,
    registration date NULL,
    CONSTRAINT pk_category PRIMARY KEY (id)
 );
@@ -35,51 +19,103 @@ CREATE TABLE vehicle (
    plate VARCHAR(255) NULL,
    color VARCHAR(255) NULL,
    transmission VARCHAR(255) NULL,
-   current_km VARCHAR(255) NULL,
+   current_km DOUBLE NOT NULL,
    passanger_capacity VARCHAR(255) NULL,
    trunk_capacity VARCHAR(255) NULL,
+    accessories VARCHAR(255) NULL,
    daily_rate DOUBLE NOT NULL,
+   status_vehicle SMALLINT NULL,
    registration_date date NULL,
    CONSTRAINT pk_vehicle PRIMARY KEY (id)
 );
 
 ALTER TABLE vehicle ADD CONSTRAINT FK_VEHICLE_ON_CATEGORY FOREIGN KEY (category_id) REFERENCES category (id);
 
+
 CREATE TABLE client (
   id BIGINT AUTO_INCREMENT NOT NULL,
-   person_type VARCHAR(255) NOT NULL,
-   natural_person_name VARCHAR(255) NULL,
-   cpf VARCHAR(255) NULL,
-   cnpj VARCHAR(255) NULL,
-   company_name VARCHAR(255) NULL,
-   email VARCHAR(255) NULL,
-   gender VARCHAR(255) NULL,
+   person_type VARCHAR(255) NULL,
    date_birth datetime NULL,
    cep VARCHAR(255) NULL,
    endereco VARCHAR(255) NULL,
    registration date NULL,
+   email VARCHAR(255) NULL,
+   cpf VARCHAR(255) NULL,
+   gender VARCHAR(255) NULL,
+   natural_person_name VARCHAR(255) NULL,
+   cnpj VARCHAR(255) NULL,
+   company_name VARCHAR(255) NULL,
+   drivers_license_number VARCHAR(255) NULL,
+   drivers_license_validity datetime NULL,
    CONSTRAINT pk_client PRIMARY KEY (id)
 );
+
+CREATE TABLE driver_license_categories (
+  driver_id BIGINT NOT NULL,
+   license_category VARCHAR(255) NULL
+);
+
+ALTER TABLE driver_license_categories ADD CONSTRAINT fk_driver_license_categories_on_client FOREIGN KEY (driver_id) REFERENCES client (id);
+
+CREATE TABLE accident (
+  id BIGINT AUTO_INCREMENT NOT NULL,
+   severity VARCHAR(255) NULL,
+   `description` VARCHAR(255) NOT NULL,
+   vehicle_id BIGINT NULL,
+   registration_date datetime NULL,
+   CONSTRAINT pk_accident PRIMARY KEY (id)
+);
+
+ALTER TABLE accident ADD CONSTRAINT uc_accident_description UNIQUE (`description`);
+
+ALTER TABLE accident ADD CONSTRAINT FK_ACCIDENT_ON_VEHICLE FOREIGN KEY (vehicle_id) REFERENCES vehicle (id);
+
+
+CREATE TABLE optional_item (
+  id BIGINT AUTO_INCREMENT NOT NULL,
+   name VARCHAR(255) NOT NULL,
+   rental_value DOUBLE NOT NULL,
+   quantity_available DOUBLE NOT NULL,
+   CONSTRAINT pk_optionalitem PRIMARY KEY (id)
+);
+
+ALTER TABLE optional_item ADD CONSTRAINT uc_optionalitem_name UNIQUE (name);
+
+
+CREATE TABLE delay_penalty (
+  id BIGINT NOT NULL,
+   days INT NULL,
+   percentage DOUBLE NULL,
+   category_id BIGINT NULL,
+   CONSTRAINT pk_delaypenalty PRIMARY KEY (id)
+);
+
+ALTER TABLE delay_penalty ADD CONSTRAINT FK_DELAYPENALTY_ON_CATEGORY FOREIGN KEY (category_id) REFERENCES category (id);
+
 CREATE TABLE rent (
   id BIGINT AUTO_INCREMENT NOT NULL,
-   daily_rate DECIMAL NULL,
-   total_days INT NOT NULL,
-   deposit DECIMAL NULL,
-   km_initial DECIMAL NULL,
-   km_final DECIMAL NULL,
-   create_at date NULL,
-   update_at date NULL,
+   status VARCHAR(255) NULL,
    user VARCHAR(255) NULL,
    client_id BIGINT NULL,
-   vehicle_id BIGINT NULL,
-   status VARCHAR(255) NULL,
+   vehicle_id BIGINT NOT NULL,
+   total_days INT NOT NULL,
+   security_deposit DOUBLE NULL,
+   start_date date NULL,
+   expected_completion_date date NULL,
+   finished_date date NULL,
+   daily_rate DOUBLE NULL,
+   total_value DOUBLE NULL,
+   total_value_optional_items DOUBLE NULL,
+   km_initial DOUBLE NULL,
+   km_final DOUBLE NULL,
+   accident_id BIGINT NULL,
+   create_at date NULL,
+   update_at date NULL,
    CONSTRAINT pk_rent PRIMARY KEY (id)
 );
 
+ALTER TABLE rent ADD CONSTRAINT FK_RENT_ON_ACCIDENT FOREIGN KEY (accident_id) REFERENCES accident (id);
 
 ALTER TABLE rent ADD CONSTRAINT FK_RENT_ON_CLIENT FOREIGN KEY (client_id) REFERENCES client (id);
+
 ALTER TABLE rent ADD CONSTRAINT FK_RENT_ON_VEHICLE FOREIGN KEY (vehicle_id) REFERENCES vehicle (id);
-ALTER TABLE rent MODIFY COLUMN daily_rate DECIMAL(38,2);
-ALTER TABLE rent MODIFY COLUMN deposit DECIMAL(38,2);
-ALTER TABLE rent MODIFY COLUMN km_initial DECIMAL(38,2);
-ALTER TABLE rent MODIFY COLUMN km_final DECIMAL(38,2);
--- src/main/resources/db/migration/V2__populate_tables.sql
+++ src/main/resources/db/migration/V2__populate_tables.sql
@@ -1,48 +1,68 @@
-INSERT INTO accident (severity, description, registration_date) VALUES
-('LOW', 'Accidente leve, sem feridos.', NOW()),
-('MEDIUM', 'Accidente moderado com alguns feridos.', NOW()),
-('HIGH', 'Accidente com danos significativos.', NOW());
+
+INSERT INTO category (name, description, registration) VALUES
+('Sedan', 'Carro como um sedan', '2024-01-01'),
+('Coupe', 'Carro como um Coupe', '2024-01-02'),
+('Hatch', 'Carro como um hatch', '2024-01-03'),
+('Cabriolet', 'Carro como um cabriolet', '2024-01-04'),
+('Minivan', 'Carro como um minivan', '2024-01-05'),
+('SUV', 'Carro como um SUV', '2024-01-06');
+
+INSERT INTO vehicle (
+name, manufacturer, version, category_id, url_fipe, plate, color, transmission, current_km,
+passanger_capacity, trunk_capacity, daily_rate, accessories, registration_date, status_vehicle)
+VALUES
+('C200', 'MERCEDES_BENZ', 'AVANTGARD', 1, 'https://www.carrosnaweb.com.br/fichadetalhe.asp?codigo=12345', 'XPT0G45', 'BLUE', 'AUTOMATIC', 0, 4, 0, 100.00, '2024-01-01', '{"CENTRAL_MULTIMIDIA", "AR_CONDICIONADO_DIGITAL"}', 'ACTIVE'),
+('X5', 'BMW', 'M', 6, 'https://www.carrosnaweb.com.br/fichadetalhe.asp?codigo=12346', 'ABC1D23', 'BLUE', 'AUTOMATIC', 0, 4, 0, 100.00, '2024-01-02', '{"GPS", "AIRBAGS", "VIDROS_ELETRICOS"}', 'INACTIVE'),
+('A4', 'AUDI', 'SLINE', 1, 'https://www.carrosnaweb.com.br/fichadetalhe.asp?codigo=12347', 'LKM3R17', 'BLUE', 'AUTOMATIC', 0, 4, 0, 100.00, '2024-01-03', '{"AIRBAGS", "CENTRAL_MULTIMIDIA"}', 'ACTIVE'),
+('PASSAT', 'VOLKSWAGEN', 'VARIANT', 1, 'https://www.carrosnaweb.com.br/fichadetalhe.asp?codigo=12348', 'RYZ9W83', 'BLUE', 'AUTOMATIC', 0, 4, 0, 100.00, '2024-01-04', '{"GPS", "AQUECIMENTO_BANCOS"}', 'ACTIVE'),
+('ESCORT', 'FORD', 'GL', 3, 'https://www.carrosnaweb.com.br/fichadetalhe.asp?codigo=12350', 'NHD5L81', 'BLUE', 'AUTOMATIC', 0, 4, 0, 100.00, '2024-01-05', '{"VIDROS_ELETRICOS"}', 'INACTIVE'),
+('COROLLA', 'TOYOTA', 'XEI', 1, 'https://www.carrosnaweb.com.br/fichadetalhe.asp?codigo=12351', 'ZFW6V30', 'BLUE', 'AUTOMATIC', 0, 4, 0, 100.00, '2024-01-06', '{"AIRBAGS", "GPS"}', 'ACTIVE'),
+('CIVIC', 'HONDA', 'SI', 1, 'https://www.carrosnaweb.com.br/fichadetalhe.asp?codigo=12352', 'GJH2T54', 'BLUE', 'AUTOMATIC', 0, 4, 0, 100.00, '2024-01-07', '{"AR_CONDICIONADO_DIGITAL", "GPS"}', 'ACTIVE'),
+('KICKS', 'NISSAN', 'ACTIVE', 6, 'https://www.carrosnaweb.com.br/fichadetalhe.asp?codigo=12353', 'KLP4X72', 'BLUE', 'AUTOMATIC', 0, 4, 0, 100.00, '2024-01-08', '{"CENTRAL_MULTIMIDIA", "AIRBAGS"}', 'INACTIVE'),
+('OPALA', 'CHEVROLET', 'COMODORO', 1, 'https://www.carrosnaweb.com.br/fichadetalhe.asp?codigo=12354', 'WST8Q67', 'BLUE', 'AUTOMATIC', 0, 4, 0, 100.00, '2024-01-09', '{"AQUECIMENTO_BANCOS", "VIDROS_ELETRICOS"}', 'ACTIVE'),
+('FERRARI 488', 'FERRARI', 'SPIDER', 2, 'https://www.carrosnaweb.com.br/fichadetalhe.asp?codigo=12355', 'FERR123', 'RED', 'AUTOMATIC', 0, 2, 0, 500.00, '2024-01-10', '{"GPS", "AIRBAGS"}', 'ACTIVE');
+
+
+INSERT INTO client (
+person_type, date_birth, cep, endereco, registration, email, cpf, gender, natural_person_name, cnpj,
+company_name, drivers_license_number, drivers_license_validity, license) VALUES
+('PF', '1990-05-20', '12345-678', 'Rua Exemplo, 123', '2024-01-01', 'cliente1@email.com', '123.456.789-00', 'MASCULINO',
+ 'João da Silva', NULL, NULL, '1234567890', '2025-12-31'),
+('PF', '1985-07-15', '23456-789', 'Avenida Exemplo, 456', '2024-01-02', 'cliente2@email.com', '987.654.321-00',
+'FEMININO', 'Maria Oliveira', NULL, NULL, '9876543210', '2025-12-31'),
+('PJ', NULL, '34567-890', 'Rua Comercial, 789', '2024-01-03', 'empresa1@email.com', NULL, 'MASCULINO',
+NULL, '12.345.678/0001-90', 'Empresa Exemplo Ltda', '2345678901', '2025-12-31'),
+('PJ', NULL, '45678-901', 'Avenida Corporativa, 101', '2024-01-04', 'empresa2@email.com', NULL, 'FEMININO', NULL, '98.765.432/0001-01', 'Corporation Exemplo S.A.', '1098765432', '2025-12-31');
+
+INSERT INTO driver_license_categories (driver_id, license_category) VALUES
+(1, 'B'),
+(1, 'A');
+
+INSERT INTO accident (severity, description, vehicle_id, registration_date) VALUES
+('LOW', 'Accidente leve, sem feridos.', 1, NOW()),
+('MEDIUM', 'Accidente moderado com alguns feridos.', 2, NOW()),
+('HIGH', 'Colisão traseira em área urbana', 3, NOW());
+
+INSERT INTO delay_penalty (id, days, percentage, category_id) VALUES
+(1, 1, 5.00, 1), -- 5% de penalidade para 1 dia de atraso na categoria 'Sedan'
+(2, 3, 10.00, 2), -- 10% de penalidade para 3 dias de atraso na categoria 'SUV'
+(3, 5, 15.00, 3); -- 15% de penalidade para 5 dias de atraso na categoria 'Hatch'
 
 INSERT INTO optional_item (name, quantity_available, rental_value) VALUES
-('Cadeira', 10, 15.50),
-('Mesa', 5, 35.00),
-('Projetor', 3, 75.00);
-
-INSERT INTO category (name, description, image, registration) VALUES
-('Sedan', 'Carro como um sedan', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.google.com%2Fsearch%2Fcss%2Fhome.css&psig=AOvVaw2-3-7-0-1-6-2w-4-5&ust=1675029628972000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNDr4n8w4oVVwAAAAAdAAAAABAD', '2024-01-01'),
-('Coupe', 'Carro como um Coupe', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.google.com%2Fsearch%2Fcss%2Fhome.css&psig=AOvVaw2-3-7-0-1-6-2w-4-5&ust=1675029628972000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNDr4n8w4oVVwAAAAAdAAAAABAD', '2024-01-02'),
-('Hatch', 'Carro como um hatch', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.google.com%2Fsearch%2Fcss%2Fhome.css&psig=AOvVaw2-3-7-0-1-6-2w-4-5&ust=1675029628972000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNDr4n8w4oVVwAAAAAdAAAAABAD', '2024-01-03'),
-('Cabriolet', 'Carro como um cabriolet', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.google.com%2Fsearch%2Fcss%2Fhome.css&psig=AOvVaw2-3-7-0-1-6-2w-4-5&ust=1675029628972000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNDr4n8w4oVVwAAAAAdAAAAABAD', '2024-01-04'),
-('Minivan', 'Carro como um minivan', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.google.com%2Fsearch%2Fcss%2Fhome.css&psig=AOvVaw2-3-7-0-1-6-2w-4-5&ust=1675029628972000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNDr4n8w4oVVwAAAAAdAAAAABAD', '2024-01-05'),
-('SUV', 'Carro como um SUV', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.google.com%2Fsearch%2Fcss%2Fhome.css&psig=AOvVaw2-3-7-0-1-6-2w-4-5&ust=1675029628972000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNDr4n8w4oVVwAAAAAdAAAAABAD', '2024-01-06');
-
-INSERT INTO vehicle (name, manufacturer, version, category_id, url_fipe, plate, color, transmission, current_km, passanger_capacity, trunk_capacity, daily_rate, registration_date) VALUES
-('C200', 'MERCEDES_BENZ', 'AVANTGARD', '1', 'https://www.carrosnaweb.com.br/fichadetalhe.asp?codigo=12345', 'XPT0G45', 'BLUE', 'AUTOMATIC', 0, 4, 0, 100.00, '2024-01-01'),
-('X5', 'BMW', 'M', '6', 'https://www.carrosnaweb.com.br/fichadetalhe.asp?codigo=12346', 'ABC1D23', 'BLUE', 'AUTOMATIC', 0, 4, 0, 100.00, '2024-01-02'),
-('A4', 'AUDI', 'SLINE', '1', 'https://www.carrosnaweb.com.br/fichadetalhe.asp?codigo=12347', 'LKM3R17', 'BLUE', 'AUTOMATIC', 0, 4, 0, 100.00, '2024-01-03'),
-('PASSAT', 'VOLKSWAGEN', 'VARIANT', '1', 'https://www.carrosnaweb.com.br/fichadetalhe.asp?codigo=12348', 'RYZ9W83', 'BLUE', 'AUTOMATIC', 0, 4, 0, 100.00, '2024-01-04'),
-('ESCORT', 'FORD', 'GL', '3', 'https://www.carrosnaweb.com.br/fichadetalhe.asp?codigo=12350', 'NHD5L81', 'BLUE', 'AUTOMATIC', 0, 4, 0, 100.00, '2024-01-05'),
-('COROLLA', 'TOYOTA', 'XEI', '1', 'https://www.carrosnaweb.com.br/fichadetalhe.asp?codigo=12351', 'ZFW6V30', 'BLUE', 'AUTOMATIC', 0, 4, 0, 100.00, '2024-01-06'),
-('CIVIC', 'HONDA', 'SI', '1', 'https://www.carrosnaweb.com.br/fichadetalhe.asp?codigo=12352', 'GJH2T54', 'BLUE', 'AUTOMATIC', 0, 4, 0, 100.00, '2024-01-07'),
-('KICKS', 'NISSAN', 'ACTIVE', '6', 'https://www.carrosnaweb.com.br/fichadetalhe.asp?codigo=12353', 'KLP4X72', 'BLUE', 'AUTOMATIC', 0, 4, 0, 100.00, '2024-01-08'),
-('OPALA', 'CHEVROLET', 'COMODORO', '1', 'https://www.carrosnaweb.com.br/fichadetalhe.asp?codigo=12354', 'WST8Q67', 'BLUE', 'AUTOMATIC', 0, 4, 0, 100.00, '2024-01-09'),
-('FERRARI 488', 'FERRARI', 'SPIDER', '2', 'https://www.carrosnaweb.com.br/fichadetalhe.asp?codigo=12355', 'FERR123', 'RED', 'AUTOMATIC', 0, 2, 0, 500.00, '2024-01-10');
+('GPS', 15.00, 10),
+('Cadeirinha', 25.00, 5),
+('Seguro', 50.00, 20),
+('Assento Elevado', 20.00, 7);
 
 
-INSERT INTO client (person_type, natural_person_name, cpf, email, gender, date_birth, cep, endereco, registration) VALUES
-('PF', 'Ana Paula Souza', '123.456.789-10', 'ana.souza@email.com', 'FEMININO', '1990-08-15 00:00:00', '12345-678', 'Rua das Flores, 123, Bairro Jardim, Cidade A', '2024-01-20'),
-('PF', 'Carlos Eduardo Lima', '987.654.321-00', 'carlos.lima@email.com', 'MASCULINO', '1985-05-30 00:00:00', '54321-987', 'Avenida Central, 456, Bairro Centro, Cidade B', '2024-02-15');
+INSERT INTO rent (
+status, user, client_id, vehicle_id, total_days, security_deposit, start_date, expected_completion_date,
+finished_date, daily_rate, total_value, total_value_optional_items, km_initial, km_final, accident_id, create_at, update_at)
+VALUES(
+('PENDING', 'user1', 1, 1, 5, 200.00, '2024-11-10', '2024-11-15', NULL, 100.00, 500.00, 50.00, 0, 100, NULL, '2024-11-10', '2024-11-10'),
+('IN_PROGRESS', 'user2', 2, 2, 7, 300.00, '2024-11-05', '2024-11-12', NULL, 150.00, 1050.00, 100.00, 50, 150, NULL, '2024-11-05', '2024-11-05'),
+);
 
-INSERT INTO client (person_type, company_name, cnpj, email, cep, endereco, registration) VALUES
-('PJ', 'Loja XYZ Ltda', '12.345.678/0001-90', 'contato@lojaxyz.com', '78945-123', 'Rua Comercial, 789, Bairro Industrial, Cidade C', '2024-03-10'),
-('PJ', 'Construtora ABC S.A.', '98.765.432/0001-55', 'contato@abcconstrutora.com', '65432-098', 'Avenida das Empresas, 321, Cidade D', '2024-04-22');
-
-INSERT INTO rent (daily_rate, total_days, deposit, km_initial, km_final, create_at, update_at, user, client_id, vehicle_id, status) VALUES
-(100, 7, 500, 10000, 10500, '2024-01-10', '2024-01-10', 'usuario1', 1, 1, 'COMPLETED'),
-(120, 5, 600, 12000, 12500, '2024-02-15', '2024-02-15', 'usuario2', 2, 2, 'PENDING'),
-(150, 3, 700, 13000, 13200, '2024-03-20', '2024-03-20', 'usuario3', 3, 3, 'IN_PROGRESS'),
-(180, 10, 800, 14000, 14500, '2024-04-12', '2024-04-12', 'usuario4', 4, 4, 'COMPLETED'),
-(160, 12, 500, 15000, 15600, '2024-05-08', '2024-05-08', 'usuario5', 1, 5, 'CANCELED');
 
 
 
