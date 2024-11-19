62ddd5dc4904b2cf04a97516465adbf905fe1e04
Railan Ibraim <railanibraim3@gmail.com>
2024-11-19 18:22:01 -0300

    bug

--- src/main/java/com/br/luggycar/api/exceptions/ResourceDatabaseException.java
+++ src/main/java/com/br/luggycar/api/exceptions/ResourceDatabaseException.java
@@ -2,6 +2,6 @@ package com.br.luggycar.api.exceptions;
 
 public class ResourceDatabaseException extends Exception {
     public ResourceDatabaseException(String message) {
-        super(message, cause);
+        super(message);
     }
 }
